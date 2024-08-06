5dfdaaa7cb4d5220d2f4d806112ed8915ee176c9
Maxwell Mitchell <155778466+maxwellmitchell93@users.noreply.github.com>
2024-08-05 20:30:13 -0700

    mongoose connection,initial http request, & port setup connection

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,8 +1,26 @@
 import express from "express";
-import {PORT} from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
 
 const app = express();
-
-app.listen(PORT, () => {
-console.log (`Listening on port: ${PORT}`);
+//http request
+app.get('/',(request, response) => {
+    console.log(request)
+    return response.status (234).send('Welcome to The Reading Room.')
 });
+
+
+//mongoose connection
+mongoose
+ .connect(mongoDBURL)
+ .then(() => {
+console.log('App is connected to the database.');
+//port setup connection
+app.listen(PORT, () => {
+    console.log (`Listening on port: ${PORT}`);
+    });
+ })
+ .catch((error) => {
+    console.log(error);
+
+ });
