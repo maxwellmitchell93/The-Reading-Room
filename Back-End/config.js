66c19a769e7771919b7a788121c46fb0d891959e
Maxwell Mitchell <155778466+maxwellmitchell93@users.noreply.github.com>
2024-08-05 20:23:15 -0700

    mongodb connection

--- Back-End/config.js
+++ Back-End/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+//mongodb connection
+export const mongoDBURL = 
+'mongodb+srv://TheReadingRoomDB:UgW1ulxik0uEM8LL@the-reading-room-databa.zn3tly9.mongodb.net/?retryWrites=true&w=majority&appName=The-Reading-Room-Database'
