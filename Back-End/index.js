ee73c43056845cf7c58984c201d6fefa647e06cc
Maxwell Mitchell <155778466+maxwellmitchell93@users.noreply.github.com>
2024-08-06 20:02:05 -0700

    Http request

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,8 +1,13 @@
-import express from "express";
+import express, { response } from "express";
 
 import { PORT } from "./config.js";
 
 const app = express ();
+//http request
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status (234).send('Welcome to The Reading Room');
+});
 
 app.listen(PORT, () => {
     console.log (`app is listening on ${PORT}`);
