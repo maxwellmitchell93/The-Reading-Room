3a7251a84a3340fd436ab7655b3f0fda369228c7
Maxwell Mitchell <155778466+maxwellmitchell93@users.noreply.github.com>
2024-08-13 16:13:52 -0700

    Removed uneeded middleware in backend/index.js, added navbar and title to front end. ready for render deployment.

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -11,17 +11,18 @@ function App() {
     <>
       <div className="flex justify-center items-center space-x-4">
         <a href="https://vitejs.dev" target="_blank" className="p-4">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
         </a>
         <a href="https://react.dev" target="_blank" className="p-4">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
         </a>
       </div>
-      <h1 className="text-4xl font-bold text-center mt-8">Vite + React</h1>
+      <h1 className="text-4xl font-bold text-center mt-8">The Reading Room</h1>
       <div className="card p-8 bg-gray-100 rounded-lg shadow-md">
+        <h3 className="text-1xl font-thin text-float-right">Fiction-Nonfiction-Genres-Search</h3>
       </div>
       <p className="read-the-docs text-center mt-8">
-        Click on the Vite and React logos to learn more
+        top 10 books for 2024
       </p>
       
       <Spinner />
