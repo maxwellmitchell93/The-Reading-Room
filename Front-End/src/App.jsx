f66289a587b3534a6832153f2e03d0029d3b3a31
Morideaux <Morideaux7@gmail.com>
2024-08-12 20:07:25 -0700

    created book list functionality

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import {Routes, route} from 'react-router-dom'
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBook';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+improt DeleteBook from './pages/DeleteBook';
 
 const App = () => {
-  return (
-  <div className='border-red-400 text-white'>App</div>
-  )
-}
+  <Routes>
+    <Route path="/" elemment={<Home />} />
+    <Route path="/books/create" element={<CreateBook />} />
+    <Route path="/books/details/:id" element={<ShowBook />} />
+    <Route path="/books/edit.:id" element={<EditBook />} />
+    <Route path="/books/delete/:id" element={<DeleteBook />} />
+  </Routes>;
+};
 
-export default App
+export default App;
